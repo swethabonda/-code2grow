7391d585e1cb1afc2f401c56e6fbbc0f60a7da9b
swethabonda <swetha_bonda@srmap.edu.in>
2025-11-05 11:02:03 +0530

    Add files via upload

---
+++ day11/day11.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+    int balance = 1000, choice, amount;
+    while(1)
+    {
+        printf("Banking system: ");
+        printf("1.check balance");
+        printf("2.Withdraw money");
+        printf("3.Deposit money");
+        printf("4.Exit");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1:
+                printf("Your balance is: %d", balance);
+                break;
+                case 2:
+                printf("Enter amount to withdraw: ");
+                scanf("%d", &amount);
+                if(amount > balance)
+                {
+                    printf("Insufficient balance\n");
+                }
+                else
+                {
+                    balance -= amount;
+                    printf("Withdrawal successful");
+                }
+                break;
+            case 3:
+                printf("Enter amount to deposit: ");
+                scanf("%d", &amount);
+                balance += amount;
+                printf("Deposit successful");
+                break;
+            case 4:
+            printf("Thank you for using our banking system");
+            return 0;
+            default:
+                printf("Invalid choice");
+            }
+        }
+    return 0;
+    }
+
